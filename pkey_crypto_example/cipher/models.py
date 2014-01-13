57e4bbb3c671bc9515021537a4c69cb93b1a9a72
Oscar Javier Moreno Rey <ojmorenor@gmail.com>
2014-01-13 02:27:50 -0500

    Database model creation to store keys and generates a hash as primarykey

---
+++ pkey_crypto_example/cipher/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.core.signing import Signer
+
+# Create your models here.
+
+class EncKey(models.Model):
+    """
+    Stores, all public keys and
+    assigns as id a pregenerated
+    hash.
+    """
+    id = models.CharField(
+        max_length=256,
+        primary_key=True
+    )
+    key = models.TextField()
+
+
+@receiver(pre_save, sender=EncKey)
+def create_pk(sender, **kwargs):
+    """A signal to generate primary key
+    usign signer object from django
+    core."""
+
+    signer = Signer()
+
+    # Extracting key instance that will be saved
+    key_obj = kwargs.get('instance') 
+    signed_id = signer.sign(key_obj.key[:len(key_obj.key)/2])
+    
+    # Splits to obtain only signed hash to be defined
+    # as primary key
+    key_obj.id = signed_id.split(':')[1]
